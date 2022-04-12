7cc6206bc31b1e7c59e24f574542965330f7b491
Darshan Damre <darshandamre5@gmail.com>
2022-04-13 00:22:23 +0530

    added mock videos data

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,17 +5,499 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+    _id: "I0KSaqiv5C0",
+    title:
+      "Liquid Paper | If Bruce Lee shuffled cards... | Cardistry by Virtuoso",
+    creator: "thevirts",
+    description: `While Cardistry was first birthed centuries ago from magicians who desired to add some flourishes to their card handling, it in itself contains no magic at all. In fact, while card magic involves the concealment of movement, Cardistry is the very opposite - revolving around the expression and aesthetics of movement and form. In other words, borders often end up obscuring what the Cardist wishes to showcase.
+    
+    Unfortunately, Cardistry was always regarded as a niche within the niche of magic, and Cardists had to make do with whatever decks they could get in their hands - that is, until Virtuoso came along.`
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    _id: "S7kG641iA_g",
+    title:
+      "RISE | Crazy card moves with no CGI, strings, or magnets | Cardistry by Virtuoso",
+    creator: "thevirts",
+    description: `A note from The Virts about "RISE":
+    
+    If there's one thing we've learned from shuffling cards over the past 14 years, it's that you only get better at Cardistry as you repeatedly push yourself beyond your limits. The byproduct of that, of course, is dropping cards - again, and again, and again.
+    
+    Today, after 14 years... we still drop our cards all the time. Sure, we now drop our cards less frequently; but no matter how much better we've gotten, bending to the floor and repeatedly picking cards up covered in dust (and hair) never gets any less frustrating.`
   },
+  {
+    _id: "V4Z74-9y9Ys",
+    title: "TURBO / Cardistry by Noel Heath / Anyone 2018",
+    creator: "anyone worldwide",
+    description: `Credits: Jonas Haglund, Nikolaj Honoré, Tobias Levin, Zach Mueller, Mario Lopez, James Chesmore, Patrick Varnavas, Franco Pascali, Aviv, Oliver Sogard, Dimitri Arleri
+    
+    Noel's instagram: @noelheath
+    
+    anyoneworldwide.com
+    @anyoneww`
+  },
+  {
+    _id: "Etyn-vUg-bI",
+    title: "Tutorial: PRISM by Andrew Avila | Cardistry Touch",
+    creator: "Cardistry Touch",
+    description: `PRISM is a modern classic by Andrew Avila. This unique card structure is definitely one of the best moves of the last 10 years.
+    
+    Cardistry performed by Andrew Avila
+    Photography direction by Francois Dubois
+    Editing by François Dubois`
+  },
+  {
+    _id: "wNL7Vsw2L2A",
+    title: "Tutorial: RIFFLE FAN by Dimitri Arleri | Cardistry Touch",
+    creator: "Cardistry Touch",
+    description: `The Riffle Fan was first showcased in Dimitri's video “Silent Transition” in 2011. It has then been published in his dvd “Monument” (https://store.theory11.com/products/m...), but because the mechanics of the move evolved with the years, we've decide to release an upgraded tutorial.
+    
+    The Riffle Fan is definitely one of Dimitri's most treasured and favorite creation, and it's considered as modern classic: there is no doubt that adding it to your repertoire will help you grow as a cardist.
+    
+    Enjoy!
+
+    Cardistry performed by Dimitri Arleri
+    Photography direction by Francois Dubois
+    Editing by Ladislas Toubart`
+  },
+  {
+    _id: "GeGs2a0MUds",
+    title: "BLACK BELT | Cardistry | Zach Mueller | 2015",
+    creator: "Fontaine Cards",
+    description: `Credit to Daniel Lin, Adam Kerchman, Noel Heath, Dimitri Arleri, Christian Engblom, Franco Pascali, Franky Morales, Chase Duncan and Andrei Jikh.`
+  },
+  {
+    _id: "yQ7oeZwCrlI",
+    title: "Cardistry-Con Championship 2016 - Final: Nguyen Hoang Duy",
+    creator: "Nguyen Hoang Duy",
+    description: `instagram @nguyen.hoang.duy
+                   @di.cardistry
+gmail:FDflourisher@gmail.com
+www.dicardistry.com
+`
+  },
+  {
+    _id: "-iH9xOEpGfE",
+    title: "Cardistry-Con Championship 2019 - Final: Salvador González",
+    creator: "Salvador González",
+    description: ``
+  },
+  {
+    _id: "l_z1DK-vABE",
+    title: "SEQUENCE",
+    creator: "Av1vM",
+    description: `this video is a part of a cardistry project that can be seen here
+http://shop.dananddave.com/project-av....
+enjoy =)
+
+Music: Thriftworks - Observations 
+
+Credits:
+Spencer Clark
+Dan And Dave Buck
+Dimitri Arleri
+Devo
+Jerry
+Brendan Conner
+Alejandro Portela (Thecuso)
+Luka
+Daren Yeow
+Oliver Sogard`
+  },
+  {
+    _id: "BS459HTciy4",
+    title:
+      "WELCOME by Ladislas & Dimitri | Performance Video | Cardistry Touch",
+    creator: "Cardistry Touch",
+    description: `This is our first performance video as a company, and we're glad to share with you our latest cardistry creations!
+
+Cardistry performed by Ladislas Toubart & Dimitri Arleri
+Photography direction by Francois Dubois
+Editing by Ladislas Toubart
+Music by 20syl "Back & Forth"`
+  },
+  {
+    _id: "lc-MhmuGEX0",
+    title: "MATERIA // Cardistry by Yang Nguyen",
+    creator: "Yang Cardistry",
+    description: `Hope you enjoy this short video I made for Cardistry Touch!
+
+Playing Cards: Materia by Cardistry Touch
+Performed by Yang Nguyen`
+  },
+  {
+    _id: "2241tL_ErFQ",
+    title: "UNCUT CHALLENGE // Uncut Cardistry by Yang Nguyen",
+    creator: "Yang Cardistry",
+    description: `Playing Cards: Arctic Echo by Lotus in Hand
+Performed by Yang Nguyen
+Music: Solis Occasum by Gyvus`
+  },
+  {
+    _id: "xDI5F7fo2z4",
+    title: "CAUGHT IN THE ACT / CARDISTRY BY NIKOLAJ HONORÉ",
+    creator: "anyone worldwide",
+    description: `MOVE LIST:
+SPIN DOCTOR
+LANCELOT
+LOWRIDER
+FILLER
+DRIVE TRHOUGH
+DARLING
+SOME KINDA WERM
+2020
+DR. JONES
+NISEKO
+THREEFOLD
+
+INSPIRATION/CREDITS:
+TOBIAS LEVIN
+OLIVER SOGARD
+BRENDAN CONNER
+NOEL HEATH
+EDGAR ISAAC 
+DAN BUCK
+DAVE BUCK
+AVIV MORALY
+JUSTIN KELLY
+TASH ALAM
+ALEJANDRO PORTELA
+
+ANYONE 2019 ©
+6006 WORLDWIDE
+https://anyoneworldwide.com`
+  },
+  {
+    _id: "qqdP2aTwnqw",
+    title: `Round 4 "AKA THE FINALS" - Fontaine Trials 2021`,
+    creator: "Fontaine Cards",
+    description: `@trin.chngk
+@skykards
+@nikitalus
+
+Thank you for playing`
+  },
+  {
+    _id: "7aEFwSGHDsk",
+    title: "BLACK HOLO FONTAINES // Cardistry by Fontaine Cards // 2022",
+    creator: "Fontaine Cards",
+    description: `24 hour pre-order window starts now at http://www.fontainecards.com
+
+Orders window closes tomorrow at 12:00pm PST. Orders will ship in 8-10 weeks.
+
+edit by carter`
+  },
+  {
+    _id: "yRnqekhgedk",
+    title: `"THE FONTAINE FILES" TRAILER // Cardistry by Franco, Carter, James, Lewis, Kyle // 2020`,
+    creator: "Fontaine Cards",
+    description: `(0:28) Slushie
+(0:36) Gaucho
+(0:49) Drill bit
+(0:59) Braceface
+(1:18) Easy-Bake Oven
+(1:30) Scribblenauts
+(1:39) Double Weave Jacquard
+(1:46) Offnod
+(1:56) Eames
+(2:07) Primo`
+  },
+  {
+    _id: "ZotLsyViGvY",
+    title: `"SMALL HOURS" CARDISTRY in PARIS`,
+    creator: "anyone worldwide",
+    description: `Midnight Playing Cards available online 01.31.22 - 10:00 PST
+https://anyoneworldwide.com
+
+Follow us on instagram:
+https://www.instagram.com/anyone/
+
+Soundtrack Playlist:
+https://open.spotify.com/playlist/7eE...
+
+ANYONE WORLDWIDE
+UNUSUAL ENTERTAINMENT
+© 2022
+https://anyoneworldwide.com`
+  },
+  {
+    _id: "GdAFwEKanAA",
+    title: "THE TRIUMPH - CARDISTRY BY DAVIDE RASO",
+    creator: "anyone worldwide",
+    description: `ROCKSTAR PLAYING CARDS
+AVAILABLE THIS FRIDAY
+09/10/21
+19:00 CEST
+https://anyoneworldwide.com
+
+Davide Raso Instagram:
+https://www.instagram.com/davideraso_`
+  },
+  {
+    _id: "yxHrdohD_so",
+    title: "MIX 08 - CARDISTRY BY TOBIAS LEVIN",
+    creator: "anyone worldwide",
+    description: `MIX 08 -- ANYONE 2018
+
+MOVE LIST:
+
+00:17 SLOANE
+00:23 RACKETEER
+00:29 PANDA RING
+00:36 PESTO
+00:42 CHOP SLICE
+00:49 FREEZING STAR CUT
+00:55 HOTHEAD
+01:14 FEATURE FIT
+01:23 HOTHEAD TWO
+01:33 BULLDOZE
+01:41 ONE THROUGH 7
+
+anyoneworldwide.com`
+  },
+  {
+    _id: "gJhH7jic0UQ",
+    title: "PURE: Old-School Cardistry by Kevin Ho (2008)",
+    creator: "Kevin Ho",
+    description: `Backstory:
+---
+When I moved to California for college, I found myself using most of my free time to tinker with a deck of cards and come up with moves of my own. By the second semester, I had a decent handful of new material, plus a new HV20 camcorder that I got using the money I'd made from my book Smooth Operations. So one night, I grabbed my camera, a tripod, and a pack of Aladdins, and went down to the college gym to shoot a bunch of flourishes I'd been working on. I'd only shared this video with a small group of close friends before, but here it is now for your enjoyment.
+
+P.S. This video is probably the first time that Backdrop ever appeared on recorded media - crazy to think that the move is 12 years old already!
+
+---
+Original video description:
+---
+What happens when you strip a flourish video down to its bare essentials - one man, one deck of cards, one camera angle? In the absence of video aesthetics emerges a form of beauty on its own - something PURE.`
+  },
+  {
+    _id: "BLt0hf2XpiI",
+    title:
+      "Cardistry Tutorial - Sick and Twisted Wings of the Mockingbird (SATWOTM) by Kevin Ho",
+    creator: "Kevin Ho",
+    description: `PREREQUISITES:
+One Handed Revolution 2 Tutorial:
+https://www.bilibili.com/video/BV15t4...
+
+Around the World Tutorial:
+https://www.youtube.com/watch?v=NY1Hb...`
+  },
+  {
+    _id: "-sQFQEIuiTA",
+    title: "Cardistry Tutorial - Snake Charmer by Kevin Ho",
+    creator: "Kevin Ho",
+    description: `The cards used in the video are the ArrowDynamix v2s from my brand, Momentum. You can get them here if you dig 'em:
+http://www.momentum.cards
+
+PREREQUISITES:
+One Handed Revolution Tutorial:
+https://www.youtube.com/watch?v=OulHu...
+
+One Handed Revolution 2 Tutorial:
+https://www.bilibili.com/video/BV15t4...
+
+Z-Grip Tutorial:
+https://youtu.be/e8PAMLgLcfY?t=74`
+  },
+  {
+    _id: "JFL56Qeo-78",
+    title: "ArrowDynamix Cardistry Deck - Official Trailer",
+    creator: "Kevin Ho",
+    description: `ArrowDynamix is a pet project that I first came up with in 2015. The idea of taking arrows and putting them on cards so that this ubiquitous symbol could both enhance and inform movements in Cardistry opened up a whole new world of possibilities, and over time the concept has evolved and culminated in the cards you see being used in this video.
+
+Whether you're stacking them to create specific patterns and displays, using the arrows to act as creative hints in coming up with new moves, or if you're simply freestyling with the deck and allowing the arrows to generate random instances of apparent chaos, this deck is a tool that belongs in any Cardist's arsenal.
+
+This trailer was filmed with the help of my friend Kyle Tran, who worked together with two Cardistry world champions - Lars Mayrand and Trevor Aguiar - along with ultra-talented Cardists Matthew Beaudouin and Azlan Valentine to capture all the lovely moves featured in this video. Do give them a follow on social media if you'd like to see more of their work!`
+  },
+  {
+    _id: "uTkZOrdsl8U",
+    title: `"ANYWAY" - ArrowDynamix v3 Cardistry Deck Trailer ft. Kevin Ho`,
+    creator: "Kevin Ho",
+    description: `Hey gang! This is my first solo performance video in 5 years. It's a mix of original material, classics, and some lesser-known gems by other Cardists. ANYWAY, If you liked the cards in this video, you can get them on the Momentum web store now:
+https://momentum.cards/`
+  },
+  {
+    _id: "hpPm1lOSCPg",
+    title: "VIRUS // Cardistry Tutorial by Noel Heath",
+    creator: "Fontaine Cards",
+    description: ``
+  },
+  {
+    _id: "DP0uGdiwPxg",
+    title: "Learn the ANACONDA DRIBBLE - Tutorial",
+    creator: "Chris Ramsay",
+    description: `Today I'm going to teach you how to perform the Anaconda dribble. Special thanks to Bone Ho for allowing me to teach this.`
+  },
+  {
+    _id: "zdfWIgDe9Gw",
+    title: "Cardistry | POWER PLAY by Daniel Lin",
+    creator: "lotusinhand",
+    description: `Moves created and performed by Daniel Lin
+Filmed by Kurtis Lin
+Edited by Daniel Lin & Kurtis Lin
+Written by Kurtis Lin & Daniel Lin
+Music by Ryoji Ikeda
+
+Move List:
+0:12 - Ghandi's Flip Flop
+0:19 - Two Hand Ghandi's Flip Flop
+0:28 - Warp
+0:41 - Gravity
+0:48 - Miltank
+0:53 - Mosaic
+1:05 - Mudslide
+1:13 - Mudslide 2
+1:22 - Double Instant Erection
+1:33 - To and Fro
+1:38 - Hide and Seek
+1:43 - Newton's Cradle
+1:53 - Aethera 2
+2:00 - Parallax 2
+2:16 - Daedalus
+2:24 - Lever
+2:39 - Parasite 3
+2:49 - Reversybil
+3:02 - Swerve`
+  },
+  {
+    _id: "nzZuh2PhbXw",
+    title: "SELFISH | Cardistry by PATRICK VARNAVAS | 2020",
+    creator: "Best Cardist Alive",
+    description: `- MOVE LIST -
+SAINT ELSEWHERE
+HOUND DOG
+MORALS
+HEY HEY
+SCARAB
+SEEING DOUBLE
+DOCTOR STRANGE
+GRIP TWIRL
+QUAKE
+METRONOME
+PAYOLA
+MAGNA CARTA
+V FOR VARNAVAS
+VOD
+PAT ON THE BACK
+OUT OF HAND
+SEEING TRIPLE`
+  },
+  {
+    _id: "_BcGZbFNM0g",
+    title: "WATERBEND - Cardistry Tutorial",
+    creator: "Best Cardist Alive",
+    description: `WATERBEND is a brand new insane cascade flourish by Joe Feldpausch that curves cards as they fall through your fingers.  
+Go show Joe some love for teaching such an awesome move for free!`
+  },
+  {
+    _id: "0l7SuEq0Ldo",
+    title: "From the mothership",
+    creator: "Эдуард Риттер",
+    description: `Чудо`
+  },
+  {
+    _id: "bdMEZtSneF8",
+    title: "GOTHENBURG SHUFFLE | Cardistry | Noel Heath | 2016",
+    creator: "Fontaine Cards",
+    description: `Filmed by Bizau Cristian, edited by Noel Heath. 
+
+Credit:
+zach mueller, jonas haglund, birger karlsson, aviv moraly, dimitri arleri, nikolaj, huron low`
+  },
+  {
+    _id: "Zi-2FzZ1rd8",
+    title: "The Fontaine Fam | Cardistry | 2015",
+    creator: "Fontaine Cards",
+    description: `Featured Cardists:
+(The Fontaine Fam)
+Franky Morales - @frankymeows
+Noel Heath - @NoelHeath
+Chase Duncan - @readysetcards
+Zach Mueller - @zachmonky
+
+Follow @fontainecards for anything and errything Fontaine.
+
+More info on The Fontaine Fam Europe Tour will be announced in the coming weeks.`
+  },
+  {
+    _id: "NObQp8r4Wzg",
+    title: `"FLOATING CARDS" | Cardistry | Zach Mueller`,
+    creator: "StatusSilver",
+    description: `Performance by Zach Mueller`
+  },
+  {
+    _id: "Zwhkz9QfY14",
+    title: "ANYONE - CARDISTRY - TOKYO - LAST MINUTE",
+    creator: "anyone worldwide",
+    description: `Anyone who tries to win on time, will see it's only the last minute that counts.`
+  },
+  {
+    _id: "BH9DRqOGLAg",
+    title: "JETPACKETS │ Cardistry",
+    creator: "dealersgrip",
+    description: `
+'JETPACKETS'
+Cardistry created and performed by Oliver Sogard.
+Video shot and edited by Tobias Levin.
+Produced by dealersgrip.
+Shot on location in Copenhagen, Denmark.
+Music by X&G - "4am".
+
+Credits / Inspiration:
+Nikolaj Honoré
+Kevin Ho
+Patrick Varnavas
+Tobias Levin
+Dimitri Arleri
+Dom Corrado
+Zach Mueller
+Chris Hestnes`
+  },
+  {
+    _id: "5MiqDxD_SJg",
+    title: "Cardistry Tutorial — RETRIGGER by Oliver Sogard",
+    creator: "dealersgrip",
+    description: `Retrigger is a one-handed continuous cut with universal capabilities. Learn the original Retrigger along with the infamous four-packet variation in our latest in-depth verbal tutorial.
+
+Overview:
+Performance
+Introduction
+Step one - Opener
+Step two - Get-ready
+Step three - Split
+Step four - Replacement
+Step five - Closer
+Variation - Four-packet version [Re-Retrigger]
+Uncut performances
+Conclusion
+
+RETRIGGER
+Created and explained by Oliver Sogard
+Inspired by Nikolaj Pedersen's "Trigger" [First published in 'New York Notes', 2015]
+
+Additional camerawork by Henrik Forberg
+
+Music by Woolcoin - "Konane"`
+  },
+  {
+    _id: "JuuBas8A0-A",
+    title: "Cardistry-Con Championship 2016 - Round 2: Birger Karlsson (Uncut)",
+    creator: "Birger Karlsson",
+    description: `Credits: 
+Dennis Jin
+Thomas Maten
+Nikolaj Pedersen
+Henrik Forberg
+Dan & Dave Buck
+Dimitri Arleri
+Nick Stumphauzer 
+Alejandro Portela
+Jonas Haglund
+Liam Levanon
+
+Camera:
+Frank Sung (instagram: @frankistea)
+
+Music:
+BLVC SVND - Lament`
+  }
 ];
