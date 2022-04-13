78248bb3a5b5e621ecbb073979c7b961b6a57555
Darshan Damre <darshandamre5@gmail.com>
2022-04-14 01:32:54 +0530

    removed unused react portal

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,5 @@ export { Chips } from "./Chips/Chips";
 export { Header } from "./Header/Header";
 export { InputField } from "./InputField/InputField";
 export { Layout } from "./Layout/Layout";
-export { Portal } from "./Portal/Portal";
 export { SideBar } from "./SideBar/SideBar";
 export { VideoList } from "./VideoList/VideoList";
--- src/utils/createWrapperAndAppendToBody.js
+++
@@ -1,6 +0,0 @@
-export const createWrapperAndAppendToBody = wrapperId => {
-  const wrapperElement = document.createElement("div");
-  wrapperElement.setAttribute("id", wrapperId);
-  document.body.appendChild(wrapperElement);
-  return wrapperElement;
-};
