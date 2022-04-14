89696535b7e49eee5f315588539429a608f1fbf6
Darshan Damre <darshandamre5@gmail.com>
2022-04-14 14:25:43 +0530

    fixed import

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import { QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { BrowserRouter } from "react-router-dom";
-import { App } from "./App";
 import "./index.css";
+import { App } from "./App";
 import { queryClient } from "./react-query/client";
 import { makeServer } from "./server";
 
