0848b16fb1c8cd6881afa8b7a3b4b0c7aa8ee093
Darshan Damre <darshandamre5@gmail.com>
2022-04-16 23:02:26 +0530

    added remove from history buttons

--- src/pages/History/History.js
+++ src/pages/History/History.js
@@ -1,13 +1,24 @@
 import { Link } from "react-router-dom";
 import { VideoList } from "../../components";
+import { useRemoveAllHistory } from "../../react-query/mutations";
 import { useHistory } from "../../react-query/queries";
 
 const History = () => {
   const { data, isLoading } = useHistory();
+  const { mutate: removeAllHistory } = useRemoveAllHistory();
 
   return (
     <>
-      <h3 className="h3 mx-4 my-2">History</h3>
+      <div className="flex items-center">
+        <h3 className="h3 mx-4 my-2">History</h3>
+        {data?.history?.length > 0 ? (
+          <button
+            onClick={removeAllHistory}
+            className="btn btn--primary ml-auto mr-4">
+            Clear All
+          </button>
+        ) : null}
+      </div>
       {isLoading ? (
         "loading..."
       ) : data?.history?.length === 0 ? (
