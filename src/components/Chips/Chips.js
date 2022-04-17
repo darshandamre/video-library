525ce59fafa82fa80ef6d3dc24c0fea1fb2fe9b6
Darshan Damre <darshandamre5@gmail.com>
2022-04-17 12:30:32 +0530

    updated chips styles

--- src/components/Chips/Chips.js
+++ src/components/Chips/Chips.js
@@ -7,17 +7,19 @@ const Chips = () => {
   const [selected, setSelected] = useState("All");
 
   return (
-    <div className="chips py-1 fw-500">
-      {data?.categories?.map(({ id, name }) => (
-        <div
-          key={id}
-          className={`chips__item ${
-            selected === name ? "chips__item--selected" : ""
-          } cursor-pointer ml-2 py-1 px-2`}
-          onClick={() => setSelected(name)}>
-          {name}
-        </div>
-      ))}
+    <div className="chips__container py-1 fw-500">
+      <div className="chips">
+        {data?.categories?.map(({ id, name }) => (
+          <div
+            key={id}
+            className={`chips__item ${
+              selected === name ? "chips__item--selected" : ""
+            } cursor-pointer ml-2 py-1 px-2`}
+            onClick={() => setSelected(name)}>
+            {name}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
